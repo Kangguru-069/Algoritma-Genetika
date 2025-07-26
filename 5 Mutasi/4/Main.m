326b9630223391dc4f29dd16b4278cfae2d22d80
Kangguru-069 <158781351+Kangguru-069@users.noreply.github.com>
2025-07-26 17:44:54 +0700

    Main.m

--- 5 Mutasi/4/Main.m
+++ 5 Mutasi/4/Main.m
@@ -3,10 +3,10 @@ clc
 
 Subject = 'Satria Baja Hitam'
 
-subject length = length(Subject);
+subject_length = length(Subject);
 
 % Variabel Length
-variabel length = 10;
+variabel_length = 10;
 
 % Create variabel
 variabel = create_variabel(Subject,variabel_length)
