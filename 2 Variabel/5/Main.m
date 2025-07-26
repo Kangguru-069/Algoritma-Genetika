cd42975aeb278618c13c679d5113531ee7834185
Kangguru-069 <158781351+Kangguru-069@users.noreply.github.com>
2025-07-26 17:40:32 +0700

    Main.m

--- 2 Variabel/5/Main.m
+++ 2 Variabel/5/Main.m
@@ -6,7 +6,7 @@ Subject = 'Satria Baja Hitam'
 subject_length = length(Subject);
 
 % Variabel Length
-variabel length = 10;
+variabel_length = 10;
 
 variabel = create_variabel(Subject,variabel_length)
 
