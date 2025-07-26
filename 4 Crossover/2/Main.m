f281de10731def4a647e0287cf84ddd0e99ecc9b
Kangguru-069 <158781351+Kangguru-069@users.noreply.github.com>
2025-07-26 17:43:11 +0700

    Main.m

--- 4 Crossover/2/Main.m
+++ 4 Crossover/2/Main.m
@@ -6,7 +6,7 @@ Subject = 'Satria Baja Hitam'
 subject_length = length(Subject);
 
 % Variabel Length
-variabel length = 10;
+variabel_length = 10;
 
 % Create variabel
 variabel = create_variabel(Subject,variabel_length)
