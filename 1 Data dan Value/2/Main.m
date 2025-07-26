7d7d3bc657c3443e9048d9d4ac1d064147ea4b0c
Kangguru-069 <158781351+Kangguru-069@users.noreply.github.com>
2025-07-26 17:33:44 +0700

    Main.m

--- 1 Data dan Value/2/Main.m
+++ 1 Data dan Value/2/Main.m
@@ -3,7 +3,7 @@ clc
 
 Subject = 'Satria Baja Hitam'
 
-subject length = length(Subject);
+subject_length = length(Subject);
 
 new_data = create_data(subject_length);
 variabel = calculate_variabel(new_data,Subject);
